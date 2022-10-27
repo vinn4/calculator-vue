364b57032a221c8f24539850ce764c72dfc192cb
Vinicius Alexandre Stresser <78008324+vinn4@users.noreply.github.com>
2022-10-27 09:22:39 -0300

    Initial commit

---
+++ calculator-vue/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <h1>Hello Vue</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+@font-face {
+    font-family: "robotoMono-thin";
+    src: url('./fonts/RobotoMono-Thin.ttf');
+}
+*{
+    font-family: 'robotoMono', monospace;
+}
+body{
+    margin:0
+}
+#app{
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    color: #fff;
+    background: linear-gradient(to right,rgb(83,105,118), rgb(41,46,73));
+}
+</style>
